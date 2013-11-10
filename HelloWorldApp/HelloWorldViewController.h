739090c6b9a9c4780b5e9c235a83e9a426d7d3e3
NanDamhuis <Nan@MacBook-Pro-van-Nan.local>
2013-11-10 13:35:12 +0100

    HelloWorldApp
    A simple Hello World App in Objective-C

---
+++ HelloWorldApp/HelloWorldViewController.h
@@ -0,0 +1,16 @@
+//
+//  HelloWorldViewController.h
+//  HelloWorldApp
+//
+//  Created by Nan Damhuis on 10-11-13.
+//  Copyright (c) 2013 Nan Damhuis. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HelloWorldViewController : UIViewController
+
+-(IBAction)showMessage;
+
+
+@end
---
+++ HelloWorldApp/HelloWorldViewController.m
@@ -0,0 +1,47 @@
+//
+//  HelloWorldViewController.m
+//  HelloWorldApp
+//
+//  Created by Nan Damhuis on 10-11-13.
+//  Copyright (c) 2013 Nan Damhuis. All rights reserved.
+//
+
+#import "HelloWorldViewController.h"
+
+@interface HelloWorldViewController ()
+
+@end
+
+@implementation HelloWorldViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+- (IBAction)showMessage
+{
+    UIAlertView *helloWorldAlert = [[UIAlertView alloc]
+                                    initWithTitle:@"Dit is een pop-up" message:@"Hello, World!" delegate:nil cancelButtonTitle:@"OK" otherButtonTitles:nil];
+    
+    // Display the Hello World Message
+    [helloWorldAlert show];
+}
+
+@end
